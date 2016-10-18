649f58f09460571005e4ac6c7c20bdb9a06ce755
David Raymond <raymondd@vt.edu>
2016-10-18 16:03:03 -0400

    Deleted erroneous spaces in curl command; added nmap to apt-get command

--- range_demo2.sh
+++ range_demo2.sh
@@ -1,5 +1,5 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install rsh-client rpcbind nfs-common smbclient 
-curl https://raw.githubusercontent.com/rapid7/metasploit- omnibus/master/config/templates/metasploit-framework- wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+sudo apt-get -y install rsh-client rpcbind nfs-common smbclient nmap
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 
